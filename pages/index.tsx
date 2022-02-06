a10df73c87735c4809e0ff69fb36b355ab60f8ef
Caleb Leung <caleb.leungg@gmail.com>
2022-02-06 14:14:41 +1000

    add: tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
+import Head from 'next/head'
 import Link from 'next/link'
-import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+  <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1 className="text-6xl font-bold">
+      Ah Suh
+      <a className="text-blue-600" href="https://rafflebuzz.com.au">
+        Dud
+      </a>
+    </h1>
+
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </p>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
