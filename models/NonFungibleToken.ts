4c9328d6ab5eac970786473609cd73c04dd54f5b
Caleb Leung <caleb.leungg@gmail.com>
2022-02-06 17:13:37 +1000

    add: basic data link

---
+++ models/NonFungibleToken.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+/* NonFungibleTokenSchema will correspond to a collection in your MongoDB database. */
+const NonFungibleTokenSchema = new mongoose.Schema({
+  name: {
+    /* The name of this  NonFungibleToken*/
+
+    type: String,
+    required: [true, 'Please provide a name for this NonFungibleToken.'],
+  },
+  image: {
+    /* The owner of this NonFungibleToken */
+
+    type: String,
+    required: [true, "Please provide an image url for this NonFungibleToken."],
+  },
+  elo: {
+    /* The owner of this NonFungibleToken */
+
+    type: Number,
+    required: [true, "An NonFungibleToken must have an elo rating"]
+  },
+  type: {
+    /* The owner of this NonFungibleToken */
+
+    type: String,
+    required: [true, "Please provide a type this NonFungibleToken belongs to."],
+  }
+})
+
+export default mongoose.models.NonFungibleToken || mongoose.model('NonFungibleToken', NonFungibleTokenSchema)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import dbConnect from "../lib/dbConnect"
+import NonFungibleToken from '../models/NonFungibleToken'
 
-const IndexPage = () => (
+type Props = {
+  azuki: {
+    id: string,
+    name: string,
+    image: string,
+    type: string,
+    elo: number
+  }
+}
+
+const IndexPage = ({ azuki }: Props) => (
   <Layout title="Home | Next.js + TypeScript Example">
     <div className="flex flex-col items-center justify-center py-2">
       <Head>
@@ -15,14 +27,30 @@ const IndexPage = () => (
           Dud
         </a>
       </h1>
-
+      <code>
+        { JSON.stringify(azuki) }
+      </code>
       <p>
         <Link href="/about">
           <a>About</a>
         </Link>
       </p>
-  </div>
+    </div>
   </Layout>
 )
+
+/* Retrieves pet(s) data from mongodb database */
+export async function getServerSideProps() {
+  await dbConnect()
+
+  const result = await NonFungibleToken.find({ type: "azuki" }).limit(10)
+  const azukis = result.map((azuki) => ({
+    ...azuki.toObject(),
+    _id: azuki._id.toString()
+  }))
+  console.log(azukis)
+
+  return { props: { azuki: azukis }}
+}
 
 export default IndexPage
